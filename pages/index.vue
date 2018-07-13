96e0df51d950aae2fba890e4a2a52d3991befa7a
pikoshimame <pikoshimame@gmail.com>
2018-07-13 13:16:36 +0900

    add index image

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container section has-text-centered">
-    <router-link class="button is-medium is-rounded has-text-white" to="/next/"><i class="fa fa-caret-right"></i>&nbsp;&nbsp;ENTER</router-link>
+    <div class="content">
+      <div class="image"><router-link to="/next/"><dvac-image /></router-link></div>
+      <router-link class="button is-medium is-rounded has-text-white is-size-3-tablet is-size-5-mobile" to="/next/"><i class="fa fa-caret-right"></i>&nbsp;&nbsp;ENTER</router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import DvacImage from '~/components/index/Image'
 export default {
+  components: { DvacImage },
   layout: 'index'
 }
 </script>
@@ -16,10 +21,25 @@ export default {
   justify-content: center;
   align-items: center;
   min-height: calc(100vh - #{$footer-height} - 10rem);
+  padding-bottom: 0;
+}
+.content {
+  width: 100%;
+}
+.image {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 3rem 0;
 }
 .button {
   background-color: $key-color;
   border: none;
+  width: 250px;
+}
+@include mobile {
+  .button {
+    width: 150px;
+  }
 }
 .button:hover {
   background-color: darken($key-color, 10%);
